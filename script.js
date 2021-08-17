b87a9645c16600cd12b2cfd8a027bcb54466538f
ngvuong <vminguyen@ucdavis.edu>
2021-08-17 02:36:19 -0700

    Update project library

--- script.js
+++ script.js
@@ -9,16 +9,46 @@ let library = [
 ];
 
 const Book = {
-  title: title,
-  author: author,
-  pageTotal: pages,
-  isRead: read,
-  recommended: recommended,
+  title: this.title,
+  author: this.author,
+  pageTotal: this.pages,
+  isRead: this.read,
+  recommended: this.recommended,
 };
 
 const book1 = Object.create(Book);
 book1.title = "Test";
-function addBook() {
-  const book = prompt("Enter book to add");
-  console.log(book);
+library.push(book1);
+
+const addBtn = document.querySelector("#add-book");
+addBtn.addEventListener("click", displayForm);
+
+function displayForm() {
+  document.querySelector("#overlay").style.display = "flex";
+  const titleInput = document.querySelector("#title");
+  titleInput.focus();
+  document.querySelector(".book-input").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(titleInput.value);
+    addBook(titleInput.value);
+    off(e);
+  });
 }
+function addBook(title) {
+  const book = Object.create(Book);
+  book.title = title;
+  library.push(book);
+  displayBooks();
+}
+
+const bookList = document.querySelector(".book-list");
+function displayBooks() {
+  bookList.textContent = "";
+  library.forEach((book) => {
+    const newBook = document.createElement("li");
+    newBook.textContent = `${book.title} by ${book.author}`;
+    bookList.appendChild(newBook);
+  });
+}
+
+displayBooks();
